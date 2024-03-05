3d69c4c75114f8abcc49a2ed61669d0ce6e33ff4
alwynd <alwynd@goofishgamestudios.com>
2024-03-05 21:14:00 +0200

    new

---
+++ scripts/p4revertunchanged.bat
@@ -0,0 +1,5 @@
+cd c:\alwyn\dev\DotnetCore\P4RevertUnchanged
+
+
+rem call P4RevertUnchanged\bin\Debug\net6.0\P4RevertUnchanged.exe %1 -debug > scripts\revertunchanged.log
+call P4RevertUnchanged\bin\Debug\net6.0\P4RevertUnchanged.exe %*
